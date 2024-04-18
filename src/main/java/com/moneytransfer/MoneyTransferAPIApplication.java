d3ca7c25eb67f7fb03cc991016ea5dfd59385918
“Adriano <adriano@wso2.coml>
2024-04-18 11:46:23 -0300

    Fixing Project Structure

---
+++ src/main/java/com/moneytransfer/MoneyTransferAPIApplication.java
@@ -0,0 +1,17 @@
+package com.moneytransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MoneyTransferAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTransferAPIApplication.class, args);
+	}
+
+}
+
+//http://localhost:8080/swagger-ui/index.html
---
+++ src/main/java/com/moneytransfer/domain/Account.java
@@ -0,0 +1,37 @@
+package com.moneytransfer.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class Account {
+
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "amount")
+	private double amount;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+}
